cf5ecdf578153554a53cef8411ab72f587f6f8f4
Boris Gerretzen <borisgerretzen@gmail.com>
2023-10-23 20:19:03 +0200

    reformat

--- build/Build.cs
+++ build/Build.cs
@@ -1,44 +1,26 @@
-using System;
-using System.IO;
 using System.Linq;
 using DefaultNamespace;
 using Nuke.Common;
-using Nuke.Common.CI;
-using Nuke.Common.Execution;
 using Nuke.Common.IO;
 using Nuke.Common.ProjectModel;
-using Nuke.Common.Tooling;
 using Nuke.Common.Tools.DotNet;
 using Nuke.Common.Tools.GitVersion;
 using Nuke.Common.Utilities.Collections;
-using static Nuke.Common.EnvironmentInfo;
-using static Nuke.Common.IO.FileSystemTasks;
-using static Nuke.Common.IO.PathConstruction;
 
 class Build : NukeBuild
 {
-    /// Support plugins are available for:
-    ///   - JetBrains ReSharper        https://nuke.build/resharper
-    ///   - JetBrains Rider            https://nuke.build/rider
-    ///   - Microsoft VisualStudio     https://nuke.build/visualstudio
-    ///   - Microsoft VSCode           https://nuke.build/vscode
+    [Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")] readonly Configuration Configuration = IsLocalBuild ? Configuration.Debug : Configuration.Release;
 
-    public static int Main() => Execute<Build>(x => x.Compile);
-
-    [Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")]
-    readonly Configuration Configuration = IsLocalBuild ? Configuration.Debug : Configuration.Release;
-
-    [Parameter] string NugetApiUrl = "https://api.nuget.org/v3/index.json"; //default
-    [Parameter] [Secret] string NugetApiKey;
-
-    [Solution]
-    readonly Solution Solution;
+    [Solution] readonly Solution Solution;
 
     [GitVersion] GitVersion GitVersion;
+    [Parameter] [Secret] string NugetApiKey;
+
+    [Parameter] string NugetApiUrl = "https://api.nuget.org/v3/index.json";
 
     AbsolutePath OutputDirectory => RootDirectory / "output";
     AbsolutePath PackageDirectory => OutputDirectory / "nuget";
-    
+
     Target Clean => _ => _
         .Before(Restore)
         .Executes(() =>
@@ -104,4 +86,11 @@ class Build : NukeBuild
                     );
                 });
         });
+
+    /// Support plugins are available for:
+    /// - JetBrains ReSharper        https://nuke.build/resharper
+    /// - JetBrains Rider            https://nuke.build/rider
+    /// - Microsoft VisualStudio     https://nuke.build/visualstudio
+    /// - Microsoft VSCode           https://nuke.build/vscode
+    public static int Main() => Execute<Build>(x => x.Compile);
 }
